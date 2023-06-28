c8a7fa336056a852d9929237f9319ee41e1405fe
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-06-28 18:11:36 +0530

    dom using classname

--- dom.js
+++ dom.js
@@ -1,14 +1,25 @@
 //console.log(document.URL);
-var headerTitle=document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.textContent='Product Details';
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
+//var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent='Product Details';
+//var header=document.getElementById('main-header');
+//header.style.borderBottom='solid 3px #000';
+
+//GETELEMENTBYID
+
+//var items=document.getElementsByClassName('list-group-item');
+//items[1].style.fontWeight='bold';
+//items[0].style.backgroundColor='green';
+//for(var i=0;i<items.length;i++)
+//{
+//items[i].style.fontWeight='bold';
+//}
+
+//GETELEMENTBYCLASSNAME
 
 var items=document.getElementsByClassName('list-group-item');
-items[1].style.fontWeight='bold';
-items[0].style.backgroundColor='green';
 for(var i=0;i<items.length;i++)
 {
 items[i].style.fontWeight='bold';
 }
+items[2].style.backgroundColor='green';
