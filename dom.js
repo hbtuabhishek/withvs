15b5e0ac274d8d06e86061c914c9c8bb84620ed8
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-06-28 17:59:12 +0530

    style changes

--- dom.js
+++ dom.js
@@ -1,3 +1,14 @@
 //console.log(document.URL);
 var headerTitle=document.getElementById('header-title');
 console.log(headerTitle);
+headerTitle.textContent='Product Details';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+
+var items=document.getElementsByClassName('list-group-item');
+items[1].style.fontWeight='bold';
+items[0].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
+{
+items[i].style.fontWeight='bold';
+}
